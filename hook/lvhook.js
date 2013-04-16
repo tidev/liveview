9405e4f020f6200a3a10820c92d110c379c1a44a
euforic <cs@euforic.co>
2013-04-15 17:53:41 -0700

    Should fix the following issues TIMOB-13445 TIMOB-13550 TIMOB-13552 TIMOB-13563

--- hook/lvhook.js
+++ hook/lvhook.js
@@ -25,11 +25,8 @@ exports.init = function (logger, config, cli) {
       if (cli.argv.liveview) {
         var srcFile = data.args[0],
           destFile = data.args[1];
-
         if (srcFile == path.join(this.projectDir, 'Resources', 'app.js')) {
-          data.args[1] = path.join(path.dirname(destFile), '_app.js');
-        } else if (srcFile == path.join(this.projectDir, 'Resources', '.liveview.js')) {
-          data.args[1] = path.join(path.dirname(destFile), 'app.js');
+          data.args[0] = path.join(tempdir(), 'liveview.js');
         }
       }
       finished(data);
@@ -40,22 +37,6 @@ exports.init = function (logger, config, cli) {
     pre: function (data, finished) {
       if (cli.argv.liveview) {
         data.args[0].liveview = true;
-      }
-      finished(data);
-    }
-  });
-
-  cli.addHook('build.ios.compileJsFile', {
-    pre: function (data, finished) {
-      if (cli.argv.liveview) {
-        var target = data.args[0];
-        if (target.from == path.join(this.projectDir, 'Resources', 'app.js')) {
-          target.path = '_app.js';
-          target.to = target.to.substring(0, target.to.length - 13) + '.liveview.js';
-        } else if (target.from == path.join(this.projectDir, 'Resources', '.liveview.js')) {
-          target.path = 'app.js';
-          target.to = target.to.substring(0, target.to.length - 13) + 'app.js';
-        }
       }
       finished(data);
     }
@@ -72,9 +53,8 @@ exports.init = function (logger, config, cli) {
     priority: 2000,
     post: function (build, finished) {
       if (cli.argv.liveview) {
-        var resourceDir = path.join(pwd(), 'Resources');
-        var liveviewJS = path.join(resourceDir, '.liveview.js');
-        cp('-f', __dirname +'/../build/liveview.min.js', path.join(resourceDir,'.liveview.js'));
+        var liveviewJS = path.join(tempdir(), 'liveview.js');
+        cp('-f', __dirname +'/../build/liveview.min.js', liveviewJS);
         require('dns').lookup(require('os').hostname(), function (err, add, fam) {
           sed('-i', 'FSERVER_HOST', add, liveviewJS);
           sed('-i', 'TCP_HOST', add, liveviewJS);

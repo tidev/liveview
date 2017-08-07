b71bff0f625e1947b8777e48de9e941d8533f37d
Christopher Williams <chris.a.williams@gmail.com>
2017-08-07 13:48:15 -0400

    Use grunt-appc-js to apply eslint rules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,14 @@
-var exec = require('child_process').exec;
-
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
 		mocha_istanbul: {
 			coverage: {
-				src: ['test/*.js'],
+				src: [ 'test/*.js' ],
 				options: {
 					timeout: 3500,
 					reporter: 'mocha-jenkins-reporter',
-					reportFormats: ['lcov', 'cobertura'],
+					reportFormats: [ 'lcov', 'cobertura' ],
 					ignoreLeaks: false,
 					globals: [
 						'requestSSLInitializing',
@@ -21,24 +19,20 @@ module.exports = function(grunt) {
 			}
 		},
 		clean: {
-			test: ['tmp', 'coverage'],
-			dist: ['build']
-		},
-		eslint: {
-
-			target: ['index.js', 'lib/**/!(_)*.js', 'test/*.js', 'hook/**/*.js', 'bin/*']
+			test: [ 'tmp', 'coverage' ],
+			dist: [ 'build'  ]
 		},
 		appcJs: {
 			options: {
 				force: false
 			},
 			// Don't lint lib/_head.js or lib/_tail.js because they're fragments
-			src: ['index.js', 'lib/**/!(_)*.js', 'test/*.js', 'hook/**/*.js', 'bin/*']
+			src: [ 'Gruntfile.js', 'index.js', 'lib/**/!(_)*.js', 'test/*.js', 'hook/**/*.js', 'bin/*' ]
 		},
 		bump: {
 			options: {
-				files: ['package.json'],
-				commitFiles: ['package.json'],
+				files: [ 'package.json' ],
+				commitFiles: [ 'package.json' ],
 				pushTo: 'appcelerator'
 			}
 		},
@@ -63,23 +57,23 @@ module.exports = function(grunt) {
 				compressor: 'babili',
 				// TODO Turn off mangle!
 				files: {
-					'build/liveview.min.js': ['build/liveview.js']
+					'build/liveview.min.js': [ 'build/liveview.js' ]
 				}
 			}
 		}
 	});
 
 	// Load grunt plugins for modules
-	grunt.loadNpmTasks('grunt-eslint');
+	grunt.loadNpmTasks('grunt-appc-js');
 	grunt.loadNpmTasks('grunt-bump');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-node-minify');
 	grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-	grunt.registerTask('build', ['clean:dist', 'concat:dist', 'node-minify']);
-	grunt.registerTask('lint', ['eslint']);
-	grunt.registerTask('test', ['build', 'lint', 'clean:test', 'mocha_istanbul:coverage']);
-	grunt.registerTask('default', ['build']);
+	grunt.registerTask('build', [ 'clean:dist', 'concat:dist', 'node-minify' ]);
+	grunt.registerTask('lint', [ 'appcJs' ]);
+	grunt.registerTask('test', [ 'build', 'lint', 'clean:test', 'mocha_istanbul:coverage' ]);
+	grunt.registerTask('default', [ 'build' ]);
 
 };

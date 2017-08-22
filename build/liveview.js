c1733d84bbfcde312931f40ccbc035e237a7907b
Feon Sua <fmiao@appcelerator.com>
2017-08-22 10:40:09 -0700

    fix setPrototypeOf and globalCtx undefined

--- build/liveview.js
+++ build/liveview.js
@@ -1,10 +1,13 @@
-'use strict';
-
 /*!
  * liveview Titanium CommonJS require with some Node.js love and dirty hacks
  * Copyright (c) 2013-2017 Appcelerator
  */
 (function (globalScope) {
+
+	Object.setPrototypeOf = Object.setPrototypeOf || function (obj, proto) {
+		obj.__proto__ = proto;
+		return obj;
+	};
 	/* globals Emitter */
 	/**
   * Initialize a new `Process`.
@@ -714,4 +717,4 @@
 	// Prevent display from sleeping
 
 	Titanium.App.idleTimerDisabled = true;
-})(undefined);
+})(this);
